2c914df42dfa671ec4bcb51799bd6d37c02a3f34
SarahMumbua <mumbuasarah2030@gmail.com>
2023-04-03 12:41:02 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride) {
+  let answer;
+  if (ride <= 400) {
+    answer = 'This one is on me!';
+  } else if (ride > 2000 && ride <= 2500) {
+    answer = 'I will gladly take your thirty bucks.';
+  } else if (ride > 2500) {
+    answer = 'No can do.';
+  }
+  return answer;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
